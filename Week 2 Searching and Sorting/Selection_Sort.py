68ae93bbacc716a75c467a7ab3ff5683df2e9c17
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-06 07:58:29 +0530

    Update Selection_Sort.py

--- Week 2 Searching and Sorting/Selection_Sort.py
+++ Week 2 Searching and Sorting/Selection_Sort.py
@@ -1,18 +1,18 @@
 # <---------------------- Selection Sort ---------------------->
 # <---------------------- Approach 1. Via Loops ---------------------->
-# def selection_sort(A):
-#     n = len(A)
-#     for i in range(n):
-#         min_index = i
-#         for j in range(i + 1, n):
-#             if A[j] < A[min_index]:
-#                 min_index = j
-#         A[i], A[min_index] = A[min_index], A[i]
+def selection_sort(A):
+    n = len(A)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if A[j] < A[min_index]:
+                min_index = j
+        A[i], A[min_index] = A[min_index], A[i]
         
-# A = [-5, 3, 2, 1, -3, -3, 7, 2, 2, 2]
-# selection_sort(A)
+A = [-5, 3, 2, 1, -3, -3, 7, 2, 2, 2]
+selection_sort(A)
 
-# print(A)
+print(A)
 
 # <---------------------- Approach 2. Recursion ---------------------->
 def selection_sort_rec(A, start):
