f0cd09cb54b3c696ea1e9e8d56c39532f6db4871
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-06 07:50:48 +0530

    Add recursive approach to insertion sort
    Introduced a recursive implementation of insertion sort alongside the existing loop-based approach. Added a helper function for insertion and updated test cases to demonstrate both methods.

--- Week 2 Searching and Sorting/Insertion_Sort.py
+++ Week 2 Searching and Sorting/Insertion_Sort.py
@@ -1,5 +1,5 @@
 # <---------------------- Insertion Sort ---------------------->
-
+# <---------------------- Approach 1. Via Loops ---------------------->
 def insertion_sort(A):
     n = len(A)
     for i in range(1, n):
@@ -15,5 +15,24 @@ insertion_sort(A)
 print(A)
 
 
+# <---------------------- Approach 2. Recursion ---------------------->
+def insertion_sort_rec(A, start):
+    n = len(A)
+    if start >= n - 1:
+        return 
+    insert(A, start)
+    insertion_sort_rec(A, start + 1)
+    return 
+
+def insert(A, start): # Helper Function for Insertion
+    pos = start
+    while pos > 0 and (A[pos] < A[pos - 1]):
+        A[pos], A[pos - 1] = A[pos - 1], A[pos]
+        pos = pos - 1
+        
+        
+A = [2, -3, 2, 3, 2, -5, 2, -3, 1, 7]
+insertion_sort_rec(A, start = 0)
+print(A)
 # Time Complexity: O(N ^ 2)
 # Space Complexity: O(1) (In-place sorting)
