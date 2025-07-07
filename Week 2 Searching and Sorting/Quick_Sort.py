d1caa116576e63245e036b173672a6a748de06a7
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-07 20:38:28 +0530

    Add Quick Sort implementation in Python
    Introduces a Python implementation of the Quick Sort algorithm with example usage and comments on time and space complexity.

---
+++ Week 2 Searching and Sorting/Quick_Sort.py
@@ -0,0 +1,20 @@
+# <---------------------- Quick Sort ---------------------->
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    p = arr[-1]
+    l = [x for x in arr[:-1] if x <= p]
+    r = [x for x in arr[:-1] if x > p]
+    
+    L = quick_sort(l)
+    R = quick_sort(r)
+    
+    return L + [p] + R
+
+A = [2, -3, 2, 3, 2, -5, 2, -3, 1, 7]
+A = quick_sort(A)
+print(A)
+
+# Time Complexity: O(n Log n) {With Good Pivot} Worst Case: O(n ^ 2)
+# Space Complexity: (n)
