ee0ed4405e7a3504e4e4d9b2e307422f541ef811
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-07 20:32:13 +0530

    Add merge sort implementation in Python
    Introduces a new Python file with a merge sort function and a test case demonstrating its usage.

---
+++ Week 2 Searching and Sorting/Merge_Sort.py
@@ -0,0 +1,33 @@
+# <---------------------- Merge Sort ---------------------->
+def merge_sort(arr):
+    n = len(arr)
+    
+    if n <= 1:
+        return arr
+    
+    m = n // 2
+    left = merge_sort(arr[:m])
+    right = merge_sort(arr[m:])
+    
+    sorted_array = []
+    i = j = 0
+    
+    # Merge the sorted halves
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i += 1
+        else:
+            sorted_array.append(right[j])
+            j += 1
+    
+    # Append remaining elements (if any)
+    sorted_array.extend(left[i:])
+    sorted_array.extend(right[j:])
+    
+    return sorted_array
+
+# Test the function
+A = [2, -3, 2, 3, 2, -5, 2, -3, 1, 7]
+A = merge_sort(A)
+print(A)
