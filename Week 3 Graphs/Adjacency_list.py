212f3a88478c570f2e46e901f75bd7cd3b8c796a
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-11 19:30:35 +0530

    Fix adjacency list construction and add DFS recursion
    Corrects the adjacency list creation in Adjacency_list.py by appending neighbors instead of the entire edge list. Adds DFS_recursion.py, implementing depth-first search using recursion for graph traversal.

--- Week 3 Graphs/Adjacency_list.py
+++ Week 3 Graphs/Adjacency_list.py
@@ -5,6 +5,6 @@ A = [[0, 1], [1, 2], [0, 3], [3, 4], [3, 6], [3, 7], [4, 2], [4, 5], [5, 2]]
 D = defaultdict(list)
 
 for u, v in A:
-    D[u].append(A)
+    D[u].append(v)
 
 print(D)
---
+++ Week 3 Graphs/DFS_recursion.py
@@ -0,0 +1,22 @@
+# <---------------------- Depth First Search with Recursion ---------------------->
+from collections import defaultdict
+
+A = [[0, 1], [1, 2], [0, 3], [3, 4], [3, 6], [3, 7], [4, 2], [4, 5], [5, 2]]
+D = defaultdict(list)
+
+for u, v in A:
+    D[u].append(v)
+    
+    
+def dfs_recursive(node):
+    print(node)
+    for nei_node in D[node]:
+        if nei_node not in seen:
+            seen.add(nei_node)
+            dfs_recursive(nei_node)
+            
+source = 0
+seen = set()
+seen.add(source)
+
+dfs_recursive(source)
